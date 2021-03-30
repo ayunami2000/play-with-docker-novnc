784f78f93e0a098e49caa7667cf8b3e68db5d326
ayunami2000 <spwilliamsiam@gmail.com>
2021-03-30 16:13:00 -0400

    Update alpine_qemu_reactos.sh

--- alpine_qemu_reactos.sh
+++ alpine_qemu_reactos.sh
@@ -5,7 +5,9 @@ wget -O iso.zip https://sourceforge.net/projects/reactos/files/ReactOS/0.4.13/Re
 unzip iso.zip
 git clone https://github.com/novnc/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -m 3072 \
--net nic,model=virtio -net user -cdrom $(find . -maxdepth 1 -type f -name "*.iso") \
--rtc base=localtime,clock=host -smp cores=4,threads=4 \
--usbdevice tablet -vga vmware
+#qemu-system-x86_64 -m 3072 \
+#-net nic,model=virtio -net user -cdrom $(find . -maxdepth 1 -type f -name "*.iso") \
+#-rtc base=localtime,clock=host -smp cores=4,threads=4 \
+#-usbdevice tablet -vga vmware
+qemu-system-x86_64 -L . -vnc :0 -m 3072 \
+-net nic,model=virtio -net user -cdrom $(find . -maxdepth 1 -type f -name "*.iso")
