5e5046524c7146f82ca4812ca34256f2a19c7703
ayunami2000 <spwilliamsiam@gmail.com>
2021-03-30 15:56:29 -0400

    Create alpine_minecraft.sh

---
+++ alpine_minecraft.sh
@@ -0,0 +1,10 @@
+apk update
+apk add openjdk8-jre xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
+apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+git clone https://github.com/novnc/noVNC
+git clone https://github.com/mindstorm38/portablemc
+./noVNC/utils/launch.sh --listen 80 &
+Xvfb +extension GLX &
+export DISPLAY=:0
+x11vnc -noshm -geometry 900x720 -shared -forever &
+yes y | python3 ./portablemc/portablemc.py start -u docker
