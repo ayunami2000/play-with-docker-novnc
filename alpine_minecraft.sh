0c5aeb8c3efc7a4eda6bb47ca8d02f335b3cc44c
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-02 13:11:59 -0400

    Update alpine_minecraft.sh

--- alpine_minecraft.sh
+++ alpine_minecraft.sh
@@ -1,7 +1,7 @@
 apk update
 apk add openjdk8-jre xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
-git clone https://github.com/novnc/noVNC
+git clone https://github.com/ayunami2000/noVNC
 git clone https://github.com/mindstorm38/portablemc
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb +extension GLX &
