bfe5879ce67d873878502373edae527f76b7d306
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-02 13:11:50 -0400

    Update alpine_fluxbox.sh

--- alpine_fluxbox.sh
+++ alpine_fluxbox.sh
@@ -2,7 +2,7 @@ apk update
 apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
 apk add xterm #chromium
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
-git clone https://github.com/novnc/noVNC
+git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb +extension GLX &
 export DISPLAY=:0
