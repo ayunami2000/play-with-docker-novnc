b65973c9f6dd425af8688ec353259afb9ec4f97e
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-20 14:40:33 -0400

    Update alpine_qemu_win2k.sh

--- alpine_qemu_win2k.sh
+++ alpine_qemu_win2k.sh
@@ -3,7 +3,7 @@ apk update
 apk add qemu-img qemu-system-x86_64 python2 p7zip
 apk --no-cache add procps
 qemu-img create -f raw hda.img 5G
-wget https://cloudflare-ipfs.com/ipfs/QmRYFE2mvasp6qhk6EiLsp6kRgqAyBMJa7PByVrvGq3SRn/Microsoft%20Windows%202000%20Professional%20(5.00.2195.6717.sp4).7z
+wget "https://cloudflare-ipfs.com/ipfs/QmRYFE2mvasp6qhk6EiLsp6kRgqAyBMJa7PByVrvGq3SRn/Microsoft%20Windows%202000%20Professional%20(5.00.2195.6717.sp4).7z"
 7z e "./Microsoft Windows 2000 Professional (5.00.2195.6717.sp4).7z"
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
