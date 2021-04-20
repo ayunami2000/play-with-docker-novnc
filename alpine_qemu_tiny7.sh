915baa2d1e7d08a7c72d5095883187d632349ee6
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-20 14:27:23 -0400

    Update alpine_qemu_tiny7.sh

--- alpine_qemu_tiny7.sh
+++ alpine_qemu_tiny7.sh
@@ -2,7 +2,7 @@
 apk update
 apk add qemu-img qemu-system-x86_64 python2
 apk --no-cache add procps
-qemu-img create -f raw win7.img 10G
+qemu-img create -f raw hda.img 10G
 git clone https://github.com/Juvenal-Yescas/mediafire-dl
 cd mediafire-dl
 pip3 install -r requirements.txt
@@ -10,7 +10,7 @@ python3 mediafire-dl.py http://www.mediafire.com/file/sj7694734mb06jv/downloadis
 cd ..
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -vnc :0 -hda ./win7.img -m 3072 \
+qemu-system-x86_64 -vnc :0 -hda ./hda.img -m 3072 \
 -net nic,model=virtio -net user -cdrom ./mediafire-dl/downloadisofile.blogspot.com_Tiny7.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
