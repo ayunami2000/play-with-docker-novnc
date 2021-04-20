b610ede04efef457ef5aaadded53c5a2521deefe
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-20 14:38:51 -0400

    Create alpine_qemu_win2k.sh

---
+++ alpine_qemu_win2k.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apk update
+apk add qemu-img qemu-system-x86_64 python2 p7zip-full
+apk --no-cache add procps
+qemu-img create -f raw hda.img 5G
+wget https://cloudflare-ipfs.com/ipfs/QmRYFE2mvasp6qhk6EiLsp6kRgqAyBMJa7PByVrvGq3SRn/Microsoft%20Windows%202000%20Professional%20(5.00.2195.6717.sp4).7z
+7z e "./Microsoft Windows 2000 Professional (5.00.2195.6717.sp4).7z"
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+qemu-system-x86_64 -vnc :0 -hda ./hda.img -m 256 \
+-net nic,model=virtio -net user -cdrom "./EN_WIN2000_PRO_SP4.ISO" \
+-rtc base=localtime,clock=host -smp cores=4,threads=4 \
+-usbdevice tablet -vga vmware
