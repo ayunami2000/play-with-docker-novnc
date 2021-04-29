1e53bb217135d097a5cbadcc3e11016e8fa86918
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-29 13:26:51 -0400

    Create alpine_qemu_xppe.sh

---
+++ alpine_qemu_xppe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apk update
+apk add qemu-img qemu-system-x86_64 python2
+apk --no-cache add procps
+qemu-img create -f raw hda.img 5G
+git clone https://github.com/Juvenal-Yescas/mediafire-dl
+cd mediafire-dl
+pip3 install -r requirements.txt
+python3 mediafire-dl.py https://www.mediafire.com/file/gqpuqi803so5lag/BartPE.iso/file
+cd ..
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+qemu-system-x86_64 -vnc :0 -hda ./hda.img -m 3072 \
+-net nic,model=virtio -net user -cdrom ./mediafire-dl/BartPE.iso \
+-rtc base=localtime,clock=host -smp cores=4,threads=4 \
+-usbdevice tablet -vga vmware
