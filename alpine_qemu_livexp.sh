e4a9889c25a67d723771b06861fefbcfe51b6605
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-29 13:37:22 -0400

    Update alpine_qemu_livexp.sh

--- alpine_qemu_livexp.sh
+++ alpine_qemu_livexp.sh
@@ -11,6 +11,6 @@ cd ..
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 qemu-system-x86_64 -vnc :0 -m 3072 \
--net nic,model=virtio -net user -cdrom ./mediafire-dl/LiveXP.v10.11-ddlupdate.iso \
+-net nic,model=ne2k_pci -net user -cdrom ./mediafire-dl/LiveXP.v10.11-ddlupdate.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
