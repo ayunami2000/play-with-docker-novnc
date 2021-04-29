9253e9ca591a74508c776b7877d2ece247d3f50e
ayunami2000 <spwilliamsiam@gmail.com>
2021-04-29 13:50:28 -0400

    Update alpine_qemu_livexp.sh

--- alpine_qemu_livexp.sh
+++ alpine_qemu_livexp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apk update
-apk add qemu-system-x86_64 python2 unrar
+apk add qemu-img qemu-system-x86_64 python2 unrar
 apk --no-cache add procps
+qemu-img create -f raw hda.img 5G
 git clone https://github.com/Juvenal-Yescas/mediafire-dl
 cd mediafire-dl
 pip3 install -r requirements.txt
@@ -10,7 +11,7 @@ unrar e ./LiveXP.v10.11-ddlupdate__By_Engh3.rar
 cd ..
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -vnc :0 -m 3072 \
+qemu-system-x86_64 -vnc :0 -hda ./hda.img -m 3072 \
 -net nic,model=ne2k_pci -net user -cdrom ./mediafire-dl/LiveXP.v10.11-ddlupdate.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
