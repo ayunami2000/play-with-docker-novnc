8cdd42cfe3789e4224f36f11e88519de68dbba1f
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:53:16 -0400

    Update ubuntu_fluxbox.sh

--- ubuntu_fluxbox.sh
+++ ubuntu_fluxbox.sh
@@ -2,8 +2,8 @@
 apt update
 wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
 apt install -y virtualgl.deb
-apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
-apt install -y fluxbox python-minimal
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python-minimal
+apt install -y fluxbox
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
