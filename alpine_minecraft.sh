7877c9e09220bc48ef882bd5d166fd3305775be8
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 17:17:09 -0400

    Update alpine_minecraft.sh

--- alpine_minecraft.sh
+++ alpine_minecraft.sh
@@ -4,7 +4,7 @@ apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine
 git clone https://github.com/ayunami2000/noVNC
 git clone https://github.com/mindstorm38/portablemc
 ./noVNC/utils/launch.sh --listen 80 &
-Xvfb +extension GLX &
+Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 x11vnc -noshm -geometry 900x720 -shared -forever &
 yes y | python3 ./portablemc/portablemc.py start -u docker
