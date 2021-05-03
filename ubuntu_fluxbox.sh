0ce9fbcbb3597351b1510a6e23a81f778474ee75
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:17:03 -0400

    Update ubuntu_fluxbox.sh

--- ubuntu_fluxbox.sh
+++ ubuntu_fluxbox.sh
@@ -2,7 +2,7 @@
 add-apt-repository ppa:bumblebee/stable -y
 apt update
 apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
-apt install -y wine python-minimal
+apt install -y fluxbox python-minimal
 apt install -y virtualgl
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
