1de6c25a07a710a648d0e881a9d48f20d389b17f
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 17:17:02 -0400

    Update alpine_fluxbox.sh

--- alpine_fluxbox.sh
+++ alpine_fluxbox.sh
@@ -4,7 +4,7 @@ apk add xterm #chromium
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-Xvfb +extension GLX &
+Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 fluxbox &
 x11vnc -noshm -geometry 900x720 -shared -forever &
