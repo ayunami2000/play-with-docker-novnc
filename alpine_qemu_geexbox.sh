acfe93d643a70548b7e4e7057f476b1963d6c1bf
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 12:19:51 -0400

    Update alpine_qemu_geexbox.sh

--- alpine_qemu_geexbox.sh
+++ alpine_qemu_geexbox.sh
@@ -4,7 +4,7 @@ apk --no-cache add procps
 wget -O iso.iso http://sourceforge.net/projects/archiveos/files/g/geexbox/geexbox-3.1-x86_64.iso/download
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -m 3072 \
+qemu-system-x86_64 -vnc :0 -m 3072 \
 -net nic,model=virtio -net user -cdrom iso.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
