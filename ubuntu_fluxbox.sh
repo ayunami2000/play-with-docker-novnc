f0aa10fb9c00053b44a270848997718c8da9a574
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:13:00 -0400

    Create ubuntu_fluxbox.sh

---
+++ ubuntu_fluxbox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+add-apt-repository ppa:bumblebee/stable
+apt update
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
+apt install -y wine python-minimal
+apt install -y virtualgl
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+Xvfb +extension GLX &
+export DISPLAY=:0
+fluxbox &
+x11vnc -noshm -geometry 900x720 -shared -forever &
