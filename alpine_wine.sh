45dbed0b4f55466d823fdbf267d401d6621e31a8
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 11:45:48 -0400

    Create alpine_wine.sh

---
+++ alpine_wine.sh
@@ -0,0 +1,10 @@
+apk update
+apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
+apk add xterm wine
+apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+Xvfb +extension GLX &
+export DISPLAY=:0
+fluxbox &
+x11vnc -noshm -geometry 900x720 -shared -forever &
