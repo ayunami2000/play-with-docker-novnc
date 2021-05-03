4a069f92bd2cfa843053404a6482e92cd8cf7c71
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 11:47:02 -0400

    Update alpine_wine.sh

--- alpine_wine.sh
+++ alpine_wine.sh
@@ -1,6 +1,6 @@
 apk update
 apk add xvfb x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
-apk add xterm wine
+apk add wine
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
