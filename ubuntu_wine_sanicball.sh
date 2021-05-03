5e21f682abdfeafebe7998f326714e4deeff0256
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:11:13 -0400

    Update ubuntu_wine_sanicball.sh

--- ubuntu_wine_sanicball.sh
+++ ubuntu_wine_sanicball.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo DONT WORK
 sleep 10
+add-apt-repository ppa:bumblebee/stable
 apt update
-apt install -y x11vnc wine python-minimal
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
+apt install -y wine python-minimal
+apt install -y virtualgl
 git clone https://github.com/Juvenal-Yescas/mediafire-dl
 cd mediafire-dl
 pip3 install -r requirements.txt
@@ -12,5 +15,7 @@ unzip -d sanicball sanicball-v0.8.2-win.zip
 cd ..
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
+Xvfb +extension GLX &
+export DISPLAY=:0
 wine64 ./mediafire-dl/sanicball/Sanicball_v0.8.2.exe &
 x11vnc -noshm -geometry 900x720 -shared -forever &
