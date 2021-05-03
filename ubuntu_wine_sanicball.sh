a630a16e5889d10227f96b8bb1cdba41f3ae5961
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:14:02 -0400

    Update ubuntu_wine_sanicball.sh

--- ubuntu_wine_sanicball.sh
+++ ubuntu_wine_sanicball.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-add-apt-repository ppa:bumblebee/stable
+add-apt-repository ppa:bumblebee/stable -y
 apt update
 apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
 apt install -y wine python-minimal
