2e6b007b8ca47db7215559aa4a6719560fa1ce31
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 16:52:35 -0400

    Update ubuntu_wine_sanicball.sh

--- ubuntu_wine_sanicball.sh
+++ ubuntu_wine_sanicball.sh
@@ -2,11 +2,10 @@
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 apt-key add winehq.key
 apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main' -y
-#add-apt-repository ppa:bumblebee/stable -y
 apt update
 wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
 apt install -y virtualgl.deb
-apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python-minimal# virtualgl
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python-minimal
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt install -y --install-recommends winehq-stable
 git clone https://github.com/Juvenal-Yescas/mediafire-dl
