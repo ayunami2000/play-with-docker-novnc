bf945b6b51d98eec7bdce408b14a3ac59e71eac5
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 17:17:18 -0400

    Update alpine_wine.sh

--- alpine_wine.sh
+++ alpine_wine.sh
@@ -4,7 +4,7 @@ apk add wine
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-Xvfb +extension GLX &
+Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 wine64 explorer /desktop=shell,900x720 &
 x11vnc -noshm -geometry 900x720 -shared -forever &
