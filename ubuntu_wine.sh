c3556a9f6f38e57e4f4cef41fb3b2c8a867c47a1
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 13:58:06 -0400

    Create ubuntu_wine.sh

---
+++ ubuntu_wine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt install -y x11vnc wine python-minimal
+git clone https://github.com/Juvenal-Yescas/mediafire-dl
+cd mediafire-dl
+pip3 install -r requirements.txt
+python3 mediafire-dl.py https://www.mediafire.com/file/763enpozco6bxan/sanicball-v0.8.2-win.zip/file
+mkdir sanicball
+unzip -d sanicball sanicball-v0.8.2-win.zip
+cd ..
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+wine64 ./mediafire-dl/sanicball/Sanicball_v0.8.2.exe &
+x11vnc -noshm -geometry 900x720 -shared -forever &
