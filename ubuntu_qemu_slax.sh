1105c4f7c8374b583a43a1a57a1ee717b5aca530
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 12:19:14 -0400

    Update ubuntu_qemu_slax.sh

--- ubuntu_qemu_slax.sh
+++ ubuntu_qemu_slax.sh
@@ -8,3 +8,17 @@ qemu-system-x86_64 -m 3072 \
 -net nic,model=virtio -net user -cdrom iso.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
+
+
+
+#!/bin/bash
+apt update
+apt install -y qemu qemu-system-x86 python2
+qemu-img create -f raw hda.img 10G
+wget -O iso.iso http://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+qemu-system-x86_64 -vnc :0 -m 3072 \
+-net nic,model=virtio -net user -cdrom iso.iso \
+-rtc base=localtime,clock=host -smp cores=4,threads=4 \
+-usbdevice tablet -vga vmware
