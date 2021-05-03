149dbac2838e7c2437511a076a79a0147af4f6f7
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-03 13:04:38 -0400

    Create ubuntu_qemu_ipxe.sh

---
+++ ubuntu_qemu_ipxe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt install -y qemu qemu-system-x86 python-minimal
+wget -O iso.iso http://boot.ipxe.org/ipxe.iso
+git clone https://github.com/ayunami2000/noVNC
+./noVNC/utils/launch.sh --listen 80 &
+qemu-system-x86_64 -vnc :0 -m 3072 \
+-net nic,model=virtio -net user -cdrom iso.iso \
+-rtc base=localtime,clock=host -smp cores=4,threads=4 \
+-usbdevice tablet -vga vmware
