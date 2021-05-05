33da0e92779520b4172659eef629338e21e378fd
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-05 11:20:13 -0400

    Update alpine_fluxbox_audio.sh

--- alpine_fluxbox_audio.sh
+++ alpine_fluxbox_audio.sh
@@ -9,9 +9,12 @@ export DISPLAY=:0
 fluxbox &
 x11vnc -noshm -geometry 900x720 -shared -forever &
 
-apk add pulseaudio pulseaudio-alsa alsa-plugins-pulse pulseaudio-utils vlc
+apk add alsa-base pulseaudio pulseaudio-alsa alsa-plugins-pulse pulseaudio-utils vlc
 pulseaudio --start
 pacmd load-module module-null-sink sink_name=bruh
 #pacmd load-module module-loopback sink=bruh
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 cvlc -vvv pulse://bruh.monitor --sout '#transcode{acodec=mp3,ab=128,channels=2}:standard{access=http,dst=0.0.0.0:81/a.mp3}' &
+
+wget -O bruh.mp3 https://imagesharing.com/uploads/20201125/4420f20ce9b9944e87e94fbf89b261a10cab63e5.mp3
+vlc bruh.mp3 --aout=pulse
