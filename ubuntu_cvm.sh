3982b48639c8d6d6cd964cea3a7b5fa7051796d3
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-09 18:04:12 -0400

    Update ubuntu_cvm.sh

--- ubuntu_cvm.sh
+++ ubuntu_cvm.sh
@@ -8,7 +8,10 @@ wget https://github.com/ayunami2000/play-with-docker-novnc/raw/main/cvmpublic.zi
 unzip cvmpublic.zip
 rm -rf cvmpublic.zip
 chmod +x ./collab-vm-server
-echo open another terminal and wget your preferred iso: wget -O iso.iso http://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso
+echo open another terminal and wget your preferred iso. samples listed below
+echo wget -O iso.iso http://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso
+echo wget -O iso.iso https://downloads.sourceforge.net/project/archiveos/o/olive/olive-0.2.iso
+echo wget -O iso.iso http://tinycorelinux.net/12.x/x86/release/TinyCore-current.iso
 echo qemu-system-x86_64 -soundhw hda -boot d -cdrom iso.iso -m 2048 -net nic,model=virtio -net user -rtc base=localtime,clock=host -smp cores=4,threads=4 -usbdevice tablet -vga vmware
 echo setup the vm now. when done do ctrl+c to start public
 ./collab-vm-server 80
