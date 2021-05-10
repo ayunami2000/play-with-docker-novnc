282d062f34f428fe9daa27890744318aafd8c292
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-10 18:25:29 +0200

    Added better GL support so all versions work

--- ubuntu_minecraft.sh
+++ ubuntu_minecraft.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 apt update
-wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
-apt install -y ./virtualgl.deb
 apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
 apt install -y python-minimal
 apt install -y python2
-apt install -y openjdk-8-jre qt5-default
+apt install -y openjdk-8-jre qt5-default tigervnc
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-Xvfb -screen 0 900x720x24+32 +extension GLX &
-export DISPLAY=:0
-x11vnc -noshm -geometry 900x720 -shared -forever &
 useradd minecraft
 mkdir /home/mc/
 cd /home/mc/
@@ -19,6 +14,8 @@ tar -xf ./mmc.tar.gz
 cd MultiMC/
 chown -R minecraft /home/mc/
 echo '!/bin/bash' >> step2.sh
+echo 'tigervncserver -noxstartup -SecurityTypes None -geometry 1280x720 :0'
+echo 'export DISPLAY=:0'
 echo 'export HOME="/home/mc/"' >> step2.sh
 echo './MultiMC' >> step2.sh
 sudo -u minecraft bash ./step2.sh
