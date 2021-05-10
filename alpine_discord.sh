579d17e332b198ff5cd6a631b134656a8d500920
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-10 08:30:02 -0400

    Update alpine_discord.sh

--- alpine_discord.sh
+++ alpine_discord.sh
@@ -1,6 +1,7 @@
 apk update
 apk add xvfb x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+#todo: if it is impossible try getting chromium in order to install deps
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
