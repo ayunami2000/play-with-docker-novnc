a22aa9c6473b1b51dbe79bd6aee742e0ff4ce909
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-10 08:26:35 -0400

    Update ubuntu_discord.sh

--- ubuntu_discord.sh
+++ ubuntu_discord.sh
@@ -5,6 +5,7 @@ apt install -y ./virtualgl.deb
 apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri
 apt install -y python-minimal
 apt install -y python2
+apt install -y ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
