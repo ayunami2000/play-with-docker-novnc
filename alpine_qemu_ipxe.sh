e990da4dd5edf42f67aa04144827b5f339f53dfc
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-10 17:19:12 +0200

    Improved performance and stability

--- alpine_qemu_ipxe.sh
+++ alpine_qemu_ipxe.sh
@@ -4,7 +4,8 @@ apk --no-cache add procps
 wget -O iso.iso http://boot.ipxe.org/ipxe.iso
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -vnc :0 -m 3072 \
+qemu-system-x86_64 -vnc :0 \
+--enable-kvm -m 2G -device VGA,vgamem_mb=256 \
 -net nic,model=virtio -net user -cdrom iso.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
--usbdevice tablet -vga vmware
+-usbdevice tablet
