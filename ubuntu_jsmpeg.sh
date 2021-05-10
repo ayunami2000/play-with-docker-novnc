0cceca5bf7194e988eabe22276f48e4e6814932e
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-10 13:14:45 -0400

    Update ubuntu_jsmpeg.sh

--- ubuntu_jsmpeg.sh
+++ ubuntu_jsmpeg.sh
@@ -3,5 +3,5 @@ apt update
 apt install -y xvfb fluxbox xterm
 wget https://github.com/ayunami2000/play-with-docker-novnc/raw/main/jsmpeg-vnc-linux_FIX_FOR_FAKEPORT.zip
 unzip jsmpeg-vnc-linux_FIX_FOR_FAKEPORT.zip
-Xvfb :0 -screen 0 1280x1024x24
+Xvfb :0 -screen 0 1280x1024x24 &
 ./jsmpeg-vnc -display_number 0 -port 80
