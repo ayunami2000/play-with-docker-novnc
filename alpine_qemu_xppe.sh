3a24d7454b92e6eb8191f0dfc16dc73b2edb4d25
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-10 17:31:24 +0200

    Improved performance and stability

--- alpine_qemu_xppe.sh
+++ alpine_qemu_xppe.sh
@@ -10,7 +10,8 @@ python3 mediafire-dl.py https://www.mediafire.com/file/gqpuqi803so5lag/BartPE.is
 cd ..
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -vnc :0 -hda ./hda.img -m 3072 \
+qemu-system-x86_64 -vnc :0 -hda ./hda.img \
+--enable-kvm -m 2G -device VGA,vgamem_mb=256 \
 -net nic,model=ne2k_pci -net user -cdrom ./mediafire-dl/BartPE.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
--usbdevice tablet -vga vmware
+-usbdevice tablet
