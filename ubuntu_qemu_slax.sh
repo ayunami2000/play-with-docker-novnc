8f5b2bbc8af65e50b5718999f6298ffcde7ce89f
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-10 17:13:15 +0200

    Fixed bug with kernel not recognizing GPU

--- ubuntu_qemu_slax.sh
+++ ubuntu_qemu_slax.sh
@@ -7,7 +7,7 @@ wget -O iso.iso http://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 qemu-system-x86_64 -vnc :0 \
---enable-kvm -m 2G -device VGA,vgamem_mb=256 \
+--enable-kvm -m 2G -device VGA,vgamem_mb=128 \
 -net nic,model=virtio -net user -cdrom iso.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet
