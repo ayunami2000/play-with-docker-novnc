b87d5678e1d3305353a2ff65ec443d736795b4d3
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-10 17:18:38 +0200

    Improved performance and stability

--- alpine_qemu_geexbox.sh
+++ alpine_qemu_geexbox.sh
@@ -4,7 +4,8 @@ apk --no-cache add procps
 wget -O iso.iso http://sourceforge.net/projects/archiveos/files/g/geexbox/geexbox-3.1-x86_64.iso/download
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -vnc :0 -m 3072 \
+qemu-system-x86_64 -vnc :0 \
+--enable-kvm -m 2G -device VGA,vgamem_mb=256 \
 -net nic,model=virtio -net user -cdrom iso.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
