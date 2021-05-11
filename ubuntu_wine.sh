0ba484086bc0229f015b029f3e4d8cb801ab8aa3
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-11 11:22:46 -0400

    Update ubuntu_wine.sh

--- ubuntu_wine.sh
+++ ubuntu_wine.sh
@@ -10,6 +10,12 @@ apt install -y python-minimal
 apt install -y python2
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt install -y --install-recommends winehq-stable
+apt install -y cabextract unzip p7zip-full wget
+wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+clear
+echo download and setup wine using winetricks as you wish NOW. type "exit" once you are done.
+/bin/bash
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
