51581ff4c0e6fb72e7d7e9bca66b28adefe1738a
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-11 18:39:14 +0200

    Update ubuntu_raspbian_arm.sh

--- ubuntu_raspbian_arm.sh
+++ ubuntu_raspbian_arm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 apt update
-apt install -y qemu-system-arm qemu-utils
-apt install -y python2
+apt install -y qemu-system-arm qemu-utils python2 git dialo
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-apt install git dialog
 git clone https://github.com/iAmInActions/raspbian-emulation-vnc-stuff
 cd raspbian-emulation-vnc-stuff
 chmod +x ./runme.sh
 ./runme.sh
+bash --login
