01e4ee64a09dde18657cac49da8a1568fb114796
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-20 15:06:49 -0400

    Update audio.sh

--- cloudshell/audio.sh
+++ cloudshell/audio.sh
@@ -10,5 +10,5 @@ pulseaudio -D --exit-idle-time=-1
 pactl load-module module-null-sink sink_name=SpeakerOutput sink_properties=device.description="Dummy_Output"
 pacmd load-module module-virtual-source source_name=VirtualMicrophone
 cd noVNC/audio
-ffmpeg -f pulse -i default -c:a aac -f hls -hls_time 1 -hls_segment_type "mpegts" -hls_list_size 10 -hls_delete_threshold 3 -hls_flags "delete_segments+program_date_time+temp_file" -master_pl_name "manifest.m3u8" -hls_segment_filename "segment_%v_%03d.ts" "manifest_%v.m3u8"
+ffmpeg -f pulse -i default -c:a aac -f hls -hls_time 3 -hls_segment_type "mpegts" -hls_list_size 3 -hls_delete_threshold 3 -hls_flags "delete_segments+program_date_time+temp_file" -master_pl_name "manifest.m3u8" -hls_segment_filename "segment_%v_%03d.ts" "manifest_%v.m3u8"
 cd ../..
