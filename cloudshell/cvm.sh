7a87668e35bda979fa8cc3230e52de89085a5a86
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-21 09:13:52 -0400

    Update cvm.sh

--- cloudshell/cvm.sh
+++ cloudshell/cvm.sh
@@ -7,8 +7,8 @@ then
     mkdir cvm
     cd cvm
     wget https://github.com/computernewb/collab-vm-server/releases/download/1.2.10/collab-vm-server-linux-png-amd64.zip
-    unzip collab-vm-server-linux-jpeg-amd64.zip
-    rm -rf collab-vm-server-linux-jpeg-amd64.zip
+    unzip collab-vm-server-linux-png-amd64.zip
+    rm -rf collab-vm-server-linux-png-amd64.zip
     wget https://github.com/ayunami2000/play-with-docker-novnc/raw/main/cvmpublic.zip
     unzip cvmpublic.zip
     rm -rf cvmpublic.zip
